9549a19d7309a186e597f22acede566112d9026d
Lucas <lucasbrunocc880@outlook.com>
2025-08-05 20:34:15 -0300

    criando arquivos .py

---
+++ arvore_natal.py
@@ -0,0 +1,15 @@
+print("        '")
+print("       '''")
+print("      '''''")
+print("     '''''''")
+print("    '''''''''")
+print("   '''''''''''")
+print("  '''''''''''''")
+print(" '''''''''''''''")
+print("'''''''''''''''''")
+print("       '''")
+print("     '''''''")
+print("     '''''''")
+
+
+# uma arvore de natal para comecar
---
+++ tipo_variavel.py
@@ -0,0 +1,15 @@
+#tipos de variaveis
+x = 1 
+print(type(x))
+
+y = "2"
+print(type(y))
+
+z = 3.3
+print(type(z))
+
+
+# introducao ao input
+digitar = int(input("Digite um numero: "))
+print(digitar)
+
