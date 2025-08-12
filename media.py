b8e7de1b9997a7288e7897e53526733ec2880f2d
Lucas <lucasbrunocc880@outlook.com>
2025-08-12 20:10:43 -0300

    Arquivo media.py

---
+++ media.py
@@ -0,0 +1,21 @@
+nota1 = 10
+nota2 = 10
+nota3 = 10
+
+dividendo = 3
+
+dividir = (nota1 + nota2 + nota3) / dividendo
+print(dividir)
+
+'''------------------'''
+
+a = int(input("Digite o primeiro valor: "))
+b = int(input("Digite o segundo valor: "))
+c = int(input("Digite o terceiro valor: "))
+
+dividendo = 3
+soma = (a + b + c) / dividendo
+
+print("O resultado final é:", soma)
+
+'''------------------'''
