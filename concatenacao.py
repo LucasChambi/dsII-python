168236a8e08259a49b3950de524e87453d20b87b
LucasChambi <lucasbrunocc880@Outlook.com>
2025-08-19 20:16:15 -0300

    editando

--- concatenacao.py
+++ concatenacao.py
@@ -2,10 +2,12 @@ var1 = 123
 var2 = "World"
 print("Hello to the", var2, var1)
 
+# %s (string) e %d (inteiro)
 var1 = 123
 var2 = "World"
 print("Hello to the %s %d" %(var2, var1))
 
+# irá abrir duas chaves (espaço reservado na memoria)
 var1 = 123
 var2 = "World"
-print("Hello to the {} {}" .format (var2, var1))
+print("Hello to the {} {}" .format(var2, var1))
