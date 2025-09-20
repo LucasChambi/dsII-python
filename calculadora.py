eaa31a0a7b26111870fb8f6f3d632139fa96cb66
LucasChambi <lucasbrunocc880@outlook.com>
2025-09-20 01:38:39 -0300

    Atividade

---
+++ calculadora.py
@@ -0,0 +1,15 @@
+# Lucas Bruno Calle Chambi - 3DS
+
+# calculadora simples em Python
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite outro número: "))
+
+soma = num1 + num2
+subtracao = num1 - num2
+multiplicacao = num1 * num2
+divisao = num1 / num2
+
+print(f"A soma é: {soma}")
+print(f"A subtração é: {subtracao}")
+print(f"A multiplicação é: {multiplicacao}")
+print(f"A divisão é: {divisao}")
